07d6da0d1d3cb25126198e394aa9b9b2165340a8
Ricki DiCristoforo <edicrist@tulane.edu>
2018-04-19 21:27:55 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
-  config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   
