165af1400cee74d6b819b24de03fb543d652e651
Ricki DiCristoforo <edicrist@tulane.edu>
2018-04-20 00:38:11 +0000

    added blog feature

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+    before_action :find_post, only: [:edit, :update, :show, :delete]
+    
+    def index
+        @posts = Post.all
+    end
+    
+    def new 
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            flash[:notice] = "Successfully created post!"
+            redirect_to post_path(@post)
+        else
+            flash[:alert] = "Error creating new post!"
+            render :new
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @post.update_attributes(post_params)
+            flash[:notice] = "Successfully updated post!"
+             redirect_to post_path(@posts)
+        else
+            flash[:alert] = "Error updating post!"
+            render :edit
+        end
+    end
+    
+    def show
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+    
+    def find_post 
+        @post = Post.find(params[:id])
+    end
+    
+end
