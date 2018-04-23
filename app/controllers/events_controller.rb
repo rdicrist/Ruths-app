9da213cfe3485d2d9fcff69ea46f4334a9ebadc8
Ricki DiCristoforo <edicrist@tulane.edu>
2018-04-23 19:29:11 +0000

    style changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -42,6 +42,7 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
+      @event = Event.find params[:id]
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
         format.json { render :show, status: :ok, location: @event }
